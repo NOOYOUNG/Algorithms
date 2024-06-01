aa6853c6b91c54178f700aaec929a2e708b4847e
NOOYOUNG <chyooy23@gmail.com>
2024-06-01 15:27:24 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int len=arr.length;
+        double sum=0;
+        for(int i=0; i<len; i++){
+            sum+=arr[i];
+        }
+        answer=sum/len;
+        
+        return answer;
+    }
+}
