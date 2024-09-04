52f4f1c57669909d009c867bcbc251df290e5850
NOOYOUNG <chyooy23@gmail.com>
2024-09-04 21:34:37 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int arr[21];
+int N, S;
+int ans = 0;
+
+void dfs(int index, int sum) {
+    if (index == N) {
+        if (sum == S) {
+            ans++;
+        }
+        return;
+    }
+  
+    // 현재 원소를 포함시키는 경우
+    dfs(index + 1, sum + arr[index]);
+
+    // 현재 원소를 포함시키지 않는 경우
+    dfs(index + 1, sum);
+}
+
+int main() {
+    cin >> N >> S;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    dfs(0, 0);
+
+    // 빈 집합의 경우 S가 0일 때를 별도로 고려해야 할 수 있음
+    if (S == 0) {
+        ans--;  // 빈 집합이 고려된 경우, 빈 집합을 포함한 경우를 제외하기 위해 ans를 감소
+    }
+
+    cout << ans << endl;
+    return 0;
+}
