6d375103ae3ae75abd1a3746a65182075ddddc94
NOOYOUNG <chyooy23@gmail.com>
2024-09-04 18:40:22 +0900

    Update N과 M （2）.cc

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -8,15 +8,15 @@ int N, M;
 void btracking(int cnt, int start){
   if(cnt==M){
     for(int i=0; i<M; i++){
-      cout<<arr[i]<<' ';
+      cout<<arr[i]<<' '; // 현재 조합 출력
     }
     cout<<endl;
   }
-  for(int i=start; i<=N; i++){
+  for(int i=start; i<=N; i++){ // `start`부터 시작하여 각 숫자를 고려
     if(!visited[i]){
       visited[i]=true;
       arr[cnt]=i;
-      btracking(cnt+1, i+1);
+      btracking(cnt+1, i+1); // 다음 숫자가 현재 숫자보다 더 커야 함
       visited[i]=false;
     }
   }
@@ -24,6 +24,6 @@ void btracking(int cnt, int start){
 
 int main(){
   cin>>N>>M;
-  btracking(0, 1);
+  btracking(0, 1); // 숫자 1부터 시작
   return 0;
 }
