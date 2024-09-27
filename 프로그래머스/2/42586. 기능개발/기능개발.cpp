adb9dc77049ffb8463e2a091a7e8aa74509cd665
NOOYOUNG <chyooy23@gmail.com>
2024-09-27 18:49:42 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
@@ -8,20 +7,18 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     
     vector<int> days;
+    
     for(int i=0; i<progresses.size(); i++){
-        int a=0;
         if((100-progresses[i])%speeds[i]==0){
-            a=(100-progresses[i])/speeds[i];
+            days.push_back((100-progresses[i])/speeds[i]);
         }
         else{
-            a=(100-progresses[i])/speeds[i]+1;
+            days.push_back(((100-progresses[i])/speeds[i])+1);
         }
-        days.push_back(a);
     }
     
-    queue<int> q;
-    int max=days[0];
     int cnt=0;
+    int max=days[0];
     for(int i=0; i<days.size(); i++){
         if(days[i]<=max){
             cnt++;
