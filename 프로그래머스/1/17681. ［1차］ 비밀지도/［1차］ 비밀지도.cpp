c669b2073124fd4953e6aa8abfc8fd8149c77cd3
NOOYOUNG <chyooy23@gmail.com>
2024-10-03 11:13:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,10 +8,10 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     
     for(int i=0; i<n; i++){
         string row="";
-        int combined=arr1[i]|arr2[i];
+        int combined=arr1[i]|arr2[i]; // 비트 연산시 자동으로 이진화
         
-        for(int j=n-1; j>=0; j--){
-            if(combined & (1<<j)){
+        for(int j=n-1; j>=0; j--){ // 맨오른쪽부터 계산
+            if(combined & (1<<j)){ // combined의 j번째 비트가 1인지 확인 -> 1이면 참, 0이면 거짓
                 row+="#";
             }
             else{
